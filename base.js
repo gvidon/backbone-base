1fa9d688f52728874d2099d17e2bf318d2922a9b
nide <nenegoro@gmail.com>
2015-05-18 23:21:10 +0300

    Code formatting

--- base.js
+++ base.js
@@ -24,7 +24,7 @@
 			return this;
 		},
 		
-		deactivate: function() { return this.activate(false) },
+		deactivate: function() { return this.activate(false); },
 
 		initialize: function(options) {
 			this.app     = (options || {}).app;
@@ -33,7 +33,7 @@
 			this.parent  = (options || {}).parent;
 		},
 
-		isActive: function() { return ! this.$el.prop('hidden') }
+		isActive: function() { return !this.$el.prop('hidden'); }
 	});
 
 	Backbone.BaseListView = Backbone.BaseView.extend({
@@ -49,11 +49,11 @@
 		},
 		
 		// Append item view to the list node
-		addItemView: function(view, container) { (container && this.$(container) || this.$el).append(view.el); return this },
+		addItemView: function(view, container) { (container && this.$(container) || this.$el).append(view.el); return this; },
 
 		// Iterate over items, stop handling events and remove nodes
 		clear: function() {
-			_.each(this.layout.items, function(I) { (_.result(I, 'deactivate') || I).undelegateEvents(); I.remove() });
+			_.each(this.layout.items, function(I) { (_.result(I, 'deactivate') || I).undelegateEvents(); I.remove(); });
 			this.layout.items = [];
 			return this;
 		},
@@ -74,7 +74,7 @@
 			// Remove item from list once it was removed from collection
 			this.collection.on && this.collection.on('remove', function(model, collection, options) {
 				var
-					item = _.find(this.layout.items, function(I) { return I.model.id == model.id });
+					item = _.find(this.layout.items, function(I) { return I.model.id == model.id; });
 
 				this.layout.items = _.without(this.layout.items, item);
 				item.$el.fadeOut(function() { item.remove() });
